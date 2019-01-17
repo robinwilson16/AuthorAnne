32cc43086ff3420cccf21784d360b1d501033a5d
Robin Wilson <robinwilson@rwsbetas.com>
2019-01-17 01:16:03 +0000

    Completed design and content

--- AuthorAnne/Program.cs
+++ AuthorAnne/Program.cs
@@ -19,6 +19,7 @@ namespace AuthorAnne
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:5008");
     }
 }
